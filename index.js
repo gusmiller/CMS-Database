ee26d932bde19cedbbd3dffb32fc35231fe49c1b
Gus Miller <gustavo.miller@miller-hs.com>
2023-10-26 22:24:48 -0400

    Implement database pool

--- index.js
+++ index.js
@@ -8,34 +8,53 @@
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const dotenv = require('dotenv')
 
 // fs is a Node standard library package for reading and writing files
 const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+var pool = require('./utils/database');
 
-// Express middleware
+// Express middleware - The express.urlencoded() function is a built-in middleware
+// function in Express. It parses incoming requests with URL-encoded payloads and 
+// is based on a body parser.
+// https://www.geeksforgeeks.org/express-js-express-urlencoded-function/
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: dotenv.process.env.UID,
-    password: dotenv.process.env.PWD,
-    database: 'movies_db'
-  },
-  console.log(`Connected to the movies_db database.`)
-);
+// The fs.readFileSync() method is an inbuilt application programming interface of 
+// the fs module which is used to read the file and return its content. 
+// https://www.geeksforgeeks.org/node-js-fs-readfilesync-method/
+const sSql = fs.readFileSync('./db/schema.sql', 'utf-8')
+
+const init = () => {
+
+    pool.getConnection(function (err, db) {
+
+        db.query(sSql, (error, results) => {
+            if (error) {
+                console.log("Error while executing SQL Statement!")
+            } else {
+                console.log("SQL Statement successfully ran!")
+            }
+        });
+        db.release();
+    });
+    
+};
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
-  });
-  
-  app.listen(PORT, () => {
+});
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  });  
+});
+
+/**
+ * Entry point for the application. File will trigger this when it finishes
+ * loading
+ */
+init();
---
+++ utils/database.js
@@ -0,0 +1,29 @@
+/*******************************************************************
+ * Carleton Bootcamp - 2023
+ * Copyright 2023 Gustavo Miller
+ * Licensed under MIT
+ * Assignment #12 - SQL Content Management Systems (CMS)
+ * Date : 10/26/2023 10:03:23 PM
+ *******************************************************************/
+var mysql = require('mysql');
+
+// Dotenv is a zero-dependency module that loads environment variables from 
+// a .env file into process.env. Storing configuration in the environment 
+// separate from code is based on The Twelve-Factor App methodology
+// https://www.npmjs.com/package/dotenv
+require('dotenv').config();
+
+// Connection pool is similar to a cache where we store frequently accessed data. 
+// Here the data is a database connection. The goal is to achieve the reusability 
+// of the database connections instead of creating a new connection every time 
+// there is a demand for the data
+// https://codeforgeek.com/node-mysql-connection-pool-example/
+var pool = mysql.createPool({
+    connectionLimit: 100,
+    host: 'localhost',
+    user: process.env.UID,
+    password: process.env.PWD,
+    debug: false
+});
+
+module.exports = pool
