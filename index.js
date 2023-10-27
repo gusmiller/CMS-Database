20cbe127d01f514d15fe7d20f4e0e34a99085202
Gus Miller <gustavo.miller@miller-hs.com>
2023-10-26 23:06:05 -0400

    Remove express package - not required

--- index.js
+++ index.js
@@ -5,23 +5,12 @@
  * Assignment #12 - SQL Content Management Systems (CMS)
  * Date : 10/26/2023 5:06:05 PM
  *******************************************************************/
-const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
 // fs is a Node standard library package for reading and writing files
 const fs = require('fs');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-var pool = require('./utils/database');
-
-// Express middleware - The express.urlencoded() function is a built-in middleware
-// function in Express. It parses incoming requests with URL-encoded payloads and 
-// is based on a body parser.
-// https://www.geeksforgeeks.org/express-js-express-urlencoded-function/
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const pool = require('./utils/database');
 
 // The fs.readFileSync() method is an inbuilt application programming interface of 
 // the fs module which is used to read the file and return its content. 
@@ -43,15 +32,6 @@ const init = () => {
     });
     
 };
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
 
 /**
  * Entry point for the application. File will trigger this when it finishes
